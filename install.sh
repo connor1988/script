8d78cc8c4a3dfd942e084979c43ea0e105969703
connor1988 <32154724+connor1988@users.noreply.github.com>
2025-04-15 20:59:00 +0800

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,15 @@
+echo "应用BBR"
+curl https://raw.githubusercontent.com/connor1988/script/refs/heads/main/ApplyBBR.sh | bash
+
+echo "VIM 中文乱码WA"
+curl https://raw.githubusercontent.com/connor1988/script/refs/heads/main/debian-vim-utf8.sh | bash
+
+echo "安装docker v2版本"
+curl https://raw.githubusercontent.com/connor1988/script/refs/heads/main/docker-compose.sh | bash
+
+echo "安装Welcome 显示，需要提前安装vnstat"
+apt update && apt install vnstat
+curl https://raw.githubusercontent.com/connor1988/script/refs/heads/main/welcome.sh | bash
+
+echo "debian vim不能粘贴WA"
+curl | bash
