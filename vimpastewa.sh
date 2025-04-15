e200f9d59bb70d839b3a4e1861120395897b32da
connor1988 <32154724+connor1988@users.noreply.github.com>
2025-04-15 21:25:55 +0800

    Update vimpastewa.sh

--- vimpastewa.sh
+++ vimpastewa.sh
@@ -2,7 +2,6 @@
 
 find /usr/share/vim -type f -path "/usr/share/vim/vim[0-9]*/defaults.vim" | while read -r file; do
     echo "处理文件: $file"
-    #sed -i '/if has.(.*mouse.*)/,/endif/ s/^\s*set mouse=a/set mouse-=a/' "$file"
-    sed -i.bak '/if has.(.*mouse.*)/,/endif/ s/^\s*set mouse=a/set mouse-=a/' "$file"
+    sed -i.bak "/if has('mouse')/,/endif/ s/^\s*set mouse=a/set mouse-=a/" "$file"
 done
 
